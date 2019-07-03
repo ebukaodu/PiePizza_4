301c5093d7e9292f9d1316c4ddd8860fb8195dca
ebuka odu <chukas4christ@gmail.com>
2019-07-02 23:43:26 -0600

    errors

---
+++ PiePizza4/Data/PiePizzeriaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PiePizza3.Models;
+
+    public class PiePizzeriaContext : DbContext
+    {
+        public PiePizzeriaContext (DbContextOptions<PiePizzeriaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PiePizza3.Models.Pie> Pie { get; set; }
+    }
